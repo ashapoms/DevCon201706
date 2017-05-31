1697faae69dea665ed2de227fa287a1506f698f9
Alexander Shapoval <ashapoms@hotmail.com>
2017-05-31 22:20:26 +0300

    Deployment script changed

--- MAS-ARM/MAS-ARM/DeployTemplate.ps1
+++ MAS-ARM/MAS-ARM/DeployTemplate.ps1
@@ -6,7 +6,7 @@
 
 <#
  .Synopsis
-	The script creates a new resource group in Azure subscription and deploys resources based on 'openWb.json' template file and 'openWb.parameters.json' parameters file.
+	The script creates a new resource group in Azure subscription and deploys resources based on template file and parameters file.
 
  .Requirements
 	Azure PowerShell modules must be installed in order to run the script (https://www.microsoft.com/web/handlers/webpi.ashx/getinstaller/WindowsAzurePowershellGet.3f.3f.3fnew.appids).
@@ -35,9 +35,9 @@
 
 Param(
 	[string] $ResourceGroupLocation = "westeurope",
-	[string] $DeployIndex = "25",
-	[string] $ResourceGroupPrefix = "MAS-RG",
-	[string] $AzureUserName = "admin@mas201706.onmicrosoft.com",
+	[string] $DeployIndex = "61",
+	[string] $ResourceGroupPrefix = "Azure-RG",
+	[string] $AzureUserName = "admin@mytenant.onmicrosoft.com",
 	[string] $AzureUserPassword = "@zureSt@ck"
 )
 
@@ -49,15 +49,25 @@ Login-AzureRmAccount -Credential $AadCred
 # Prepare environment variables.  
 $ResourceGroupName = $ResourceGroupPrefix + $DeployIndex
 $DeploymentName = $ResourceGroupPrefix + "-Dep" + $DeployIndex
-$TemplateUri = "https://raw.githubusercontent.com/ashapoms/DevCon201706/master/MAS-ARM/MAS-ARM/azureBefore.json"
-$TemplateParameterUri = "https://raw.githubusercontent.com/ashapoms/DevCon201706/master/MAS-ARM/MAS-ARM/azureBefore.parameters.json"
+# $TemplateUri = "https://raw.githubusercontent.com/ashapoms/DevCon201706/master/MAS-ARM/MAS-ARM/azureAfter.json"
+# $TemplateParameterUri = "https://raw.githubusercontent.com/ashapoms/DevCon201706/master/MAS-ARM/MAS-ARM/azureAfter.parameters.json"
+$TemplateFile = "C:\Users\myprofile\azureAfter.json"
+$TemplateParametersFile = "C:\Users\myprofile\azureAfter.parameters.json"
 
 # Create a new resource group in given region.  
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -Force
 
-# Start a new deployment in created resource group. 
-New-AzureRmResourceGroupDeployment -Name $DeploymentName `
+# Start a new deployment in created resource group using Uri. 
+<# New-AzureRmResourceGroupDeployment -Name $DeploymentName `
                                        -ResourceGroupName $ResourceGroupName `
                                        -TemplateUri $TemplateUri `
                                        -TemplateParameterUri $TemplateParameterUri `
+                                       -Verbose
+#>
+
+# Start a new deployment in created resource group using local files.
+New-AzureRmResourceGroupDeployment -Name $DeploymentName `
+                                       -ResourceGroupName $ResourceGroupName `
+                                       -TemplateFile $TemplateFile `
+                                       -TemplateParameterFile $TemplateParametersFile `
                                        -Verbose
